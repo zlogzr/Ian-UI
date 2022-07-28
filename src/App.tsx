8e3dae2118e629b258cc2c60831f737f73c11e3a
zhangyang <2224143549@qq.com>
2022-07-29 00:58:58 +0800

    feat: 增加按钮组件

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,5 @@
+import Button, { ButtonSize, ButtonType } from '@/components/Button/button'
+
 import './App.scss'
 import { ReactComponent as Logo } from './assets/logo.svg'
 
@@ -5,6 +7,25 @@ function App() {
   return (
     <div className="app">
       <Logo width={200} />
+      {/* 大中小Button */}
+      <Button btnType={ButtonType.Primary} size={ButtonSize.Large}>
+        Large
+      </Button>
+      <Button btnType={ButtonType.Primary}>Middle</Button>
+      <Button btnType={ButtonType.Primary} size={ButtonSize.Small}>
+        Small
+      </Button>
+      {/* disabled */}
+      <Button disabled> Disabled Button </Button>
+      {/* Danger */}
+      <Button btnType={ButtonType.Danger}>Danger</Button>
+      {/* Link */}
+      <Button btnType={ButtonType.Link} href="http://www.baidu.com" target="_blank">
+        Baidu Link
+      </Button>
+      <Button btnType={ButtonType.Link} href="http://www.baidu.com" disabled>
+        Disabled Link
+      </Button>
     </div>
   )
 }
--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,7 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
 import reportWebVitals from './reportWebVitals'
+import './styles/index.scss'
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(
