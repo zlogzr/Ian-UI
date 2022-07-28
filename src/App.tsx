291168269c018fde17ebacab09cbec39be21a299
zlogzr <zylogzr@163.com>
2022-07-28 19:01:36 +0800

    feat: 初始化Ian-UI

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css'
+import { ReactComponent as Logo } from './assets/logo.svg'
+
+function App() {
+  return (
+    <div className="app">
+      <Logo width={200} />
+    </div>
+  )
+}
+
+export default App
